d23ebf5da5379529fa2222d44579fe04d8857131
Emre Demir <emreutest01@gmail.com>
2021-01-26 02:13:28 +0300

    change project

--- src/Hello.java
+++ src/Hello.java
@@ -2,5 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hi");
         //dscsdf
+        //i made some changes but do ı need to share againt?
+        //Absolutley no   just push
     }
 }
